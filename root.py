f31616099a513840708fbe47c03d2f2c3331e52f
Abhishek Guntuboina <gdsr.abhishek@gmail.com>
2025-06-11 17:14:42 +0530

    adding assets to the workspace

--- root.py
+++ root.py
@@ -2,6 +2,7 @@ from flask import Flask
 from qdrant_client import QdrantClient
 from dotenv import load_dotenv
 import os
+import fitz
 app = Flask(__name__)
 load_dotenv()
 @app.route('/',methods=['GET'])
@@ -10,6 +11,8 @@ def root():
     url=os.getenv("QDRANT_URI"), 
     api_key=os.getenv("QDRANT_API_KEY"),
     )
+    doc = fitz.open("./docs/aws-sdk-ref.pdf")
+    print(f'number of pages: {doc.page_count}')
     return qdrant_client.get_collections()
 
 
@@ -20,9 +23,9 @@ def ingestDoc():
     url=os.getenv("QDRANT_URI"), 
     api_key=os.getenv("QDRANT_API_KEY"),
 )
-
-    print(qdrant_client.get_collections())
-
+    doc = fitz.open("/docs/aws-sdk-ref.pdf")
+    print(f'number of pages: {doc.page_count}')
 
 if __name__=='__main__':
     app.run(debug=True)
+    ingestDoc()
