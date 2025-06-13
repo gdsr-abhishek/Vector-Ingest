3aef7ec48ecdf3a874b09db40e9d626ca4cdb544
Abhishek Guntuboina <gdsr.abhishek@gmail.com>
2025-06-13 22:38:29 +0530

    Revert "adding loader and chunker implementation to workspace"
    This reverts commit 1ac9085980027bdfe6a3c3111756f5ed6ee0d258.

--- root.py
+++ root.py
@@ -3,31 +3,28 @@ from qdrant_client import QdrantClient
 from dotenv import load_dotenv
 import os
 import fitz
-import glob
-from langchain.document_loaders import  PyPDFLoader
 app = Flask(__name__)
 load_dotenv()
 @app.route('/',methods=['GET'])
 def root():
-    # qdrant_client = QdrantClient(
-    # url=os.getenv("QDRANT_URI"), 
-    # api_key=os.getenv("QDRANT_API_KEY"),
-    # )
-    # doc = fitz.open("./docs/aws-sdk-ref.pdf")
-    # print(f'number of pages: {doc.page_count}')
-    # return qdrant_client.get_collections()
-    return 'Server Started successfully'
+    qdrant_client = QdrantClient(
+    url=os.getenv("QDRANT_URI"), 
+    api_key=os.getenv("QDRANT_API_KEY"),
+    )
+    doc = fitz.open("./docs/aws-sdk-ref.pdf")
+    print(f'number of pages: {doc.page_count}')
+    return qdrant_client.get_collections()
 
 
-@app.route('/ingest/',methods=['GET'])
+@app.route('/ingest/',methods=['POST'])
 def ingestDoc():
 
     qdrant_client = QdrantClient(
     url=os.getenv("QDRANT_URI"), 
     api_key=os.getenv("QDRANT_API_KEY"),
-    )
-    loader = PyPDFLoader('./docs/aws-sdk-ref.pdf')
-    print(len(loader))
+)
+    doc = fitz.open("/docs/aws-sdk-ref.pdf")
+    print(f'number of pages: {doc.page_count}')
 
 if __name__=='__main__':
     app.run(debug=True)
